519a2cb395d690406f3d95f0b8251285bca05e03
Srikanth Srider <165422078+SrikanthSrider@users.noreply.github.com>
2024-10-18 15:48:10 +0800

    Update Master_Sender_Sketch_Second.ino

--- Final_Second_Set/Master_Sender_Sketch_Second/Master_Sender_Sketch_Second.ino
+++ Final_Second_Set/Master_Sender_Sketch_Second/Master_Sender_Sketch_Second.ino
@@ -21,6 +21,8 @@ struct_message myData;
 
 esp_now_peer_info_t peerInfo;
 
+//use the below print statement to check that the connection is up and running
+
 /*void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("\r\nLast Packet Send Status:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
